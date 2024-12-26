b3db784b354e396ff26fc69b42b4aa5402bf88c4
Tami <tamigaertner@outlook.com>
2024-12-25 21:22:16 -0500

    work coding

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-# each tuple has diff len
-# need to take into account the numbs. 
-# iterate over the rows 
-# create a str  containg every "n " rom iteration
-# if n > 100 retun first letter and if n == 1 retrun all
-#
-#   Do I need to know the positions of numbers? no
-#   Do I need to compare numbers with each other? ys
-#   Do I just need to check the values?''yes
-# edge case if n < 0:
-# raise ValueERror
-#  create empty list all_chars = []
-# regual for each char  in maxtric
-#           for each char in row:
-#               add. char to all_char
-#   create empty result string
-# for position from 0 to length of all_chars:
-#     if position + 1 is divisible by n:
-#         add character at position to result   
+   
 def hidden(matrix, n):
     # Your implementation here!
     if n <= 0:
